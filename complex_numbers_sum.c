5e3f42b46694a2297178c1d042fb44b88adff17a
ENG19CS0053-TAMEEM <76118294+ENG19CS0053-TAMEEM@users.noreply.github.com>
2021-03-31 23:02:43 +0530

    Update complex_numbers_sum.c

--- complex_numbers_sum.c
+++ complex_numbers_sum.c
@@ -1,1 +1,15 @@
-//WAP to find the sum of n complex numbers using structures and 4 or more functions.
+#include<stdio.h>
+int main()
+{
+int n,a[100],sum=0;
+printf("Enter the total numbers: ");
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{
+printf("Enter a number \n");
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+printf("The sum of n numbers is %d",sum);
+return 0;
+}
